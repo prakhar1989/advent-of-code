b46fd74f3906289ec9578da620bee31b2ca2179e
Prakhar Srivastav <prakhar1989@gmail.com>
2015-12-29 01:28:20 -0500

    day3 done

---
+++ src/advent_of_code/day3.clj
@@ -0,0 +1,32 @@
+(ns advent-of-code.day3
+  (:require [clojure.java.io :as io]))
+
+(def direction
+  {\^ :north \v :south \> :east \< :west})
+
+(defn move [[x y] dir]
+  (cond
+    (= dir :north) [(inc x) y]
+    (= dir :south) [(dec x) y]
+    (= dir :west)  [x (inc y)]
+    (= dir :east)  [x (dec y)]))
+
+(defn movein-grid [input]
+  (loop [visited #{[0 0]}
+         curr [0 0]
+         dirs (map direction input)]
+    (if (empty? dirs)
+      (count visited)
+      (recur
+        (conj visited (move curr (first dirs)))
+        (move curr (first dirs))
+        (rest dirs)))))
+
+;; testcases
+(assert (= 2 (movein-grid "^v^v^v^v^v")))
+(assert (= 2 (movein-grid ">")))
+(assert (= 4 (movein-grid "^>v<")))
+
+(def input (slurp (io/resource "day3-input.txt")))
+(movein-grid input)
+
